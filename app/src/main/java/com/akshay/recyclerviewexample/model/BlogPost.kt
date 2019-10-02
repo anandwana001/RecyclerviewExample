9fb390379699080d3303118fd5d7f7f7d397f4cf
anandwana001 <akshaynandwana001@gmail.com>
2019-10-02 15:14:09 +0530

    added data source and model class

---
+++ app/src/main/java/com/akshay/recyclerviewexample/model/BlogPost.kt
@@ -0,0 +1,15 @@
+package com.akshay.recyclerviewexample.model
+
+/**
+ * Created by akshaynandwana on
+ * 02, October, 2019
+ **/
+
+data class BlogPost (
+    var title: String,
+    var body: String,
+    var image: String,
+    var username : String
+) {
+
+}
